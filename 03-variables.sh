039933b377ee49544a58943646fda268451bc832
ramadevidev7 <ramadevi.dev7@gmail.com>
2024-07-07 11:14:01 +0530

    new-files

--- 03-variables.sh
+++ 03-variables.sh
@@ -2,8 +2,8 @@
 
 
 # declaring the variables
-PERSON1=$1  # no space between variables and values
-PERSON2=$2
+PERSON1=rama  # no space between variables and values
+PERSON2=radha
 
 # reffering the variables means declare chesina variables ni use cheadam 
 echo "$PERSON1:: hello $PERSON2 how are you ?"
