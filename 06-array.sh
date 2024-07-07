9e696832733cb6711ed798e07643485bfa4a4325
ramadevidev7 <ramadevi.dev7@gmail.com>
2024-07-07 12:53:52 +0530

    new-files

--- 06-array.sh
+++ 06-array.sh
@@ -3,3 +3,9 @@
 MOVIES=("RRR" "RAM" "RAHEEM" "ROBERT")
 
 echo "first movies list ${MOVIES[0]}"
+
+echo "first actor name : ${MOVIES[1]}"
+
+echo "second actor name : ${MOVIES[2]}"
+
+echo "third actor name : ${movies[3]}"
