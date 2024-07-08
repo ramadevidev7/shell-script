858e842a17ae2b0af9c3598f7c3094de351dff90
ramadevidev7 <ramadevi.dev7@gmail.com>
2024-07-09 00:07:38 +0530

    new-files

--- 10-installation.sh
+++ 10-installation.sh
@@ -1,3 +1,17 @@
 #!/binn/bash
 
+USERID=$(id -u )
+
+# sudo userid is always 0 thats y we compare with 0 
+
+if [$USERID -ne 0]
+then 
+echo "please run this script with root access "
+
+else
+echo "you are the super user "
+fi
+
 dnf install mysql -y
+
+
