cb67d1a5795bf0cc2c48bd9df98427909b79c8e0
ramadevidev7 <ramadevi.dev7@gmail.com>
2024-07-08 21:53:18 +0530

    new-files

---
+++ 09-conditions.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+
+NUMBER=$1
+
+read NUMBER : $1
+if [$NUMBER -gt 10]
+
+then 
+echo "given number $NUMBER is greater than 10 "
+else 
+echo "given number is $NUMBER is less than 10 "
+fi
+
+
+# -gt , -lt , -eq , -ge , -le 
