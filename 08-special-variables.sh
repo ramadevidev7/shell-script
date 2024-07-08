baea1504fc968251cf1696c86027d17aecb444c2
ramadevidev7 <ramadevi.dev7@gmail.com>
2024-07-08 21:38:35 +0530

    new-files

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -3,4 +3,19 @@
 echo "all variables : $@"
 echo "all variables count : $#"
 
-# # denotes the notation of number @ means everything
+# ---  # denotes the notation of number @ means everything
+
+
+echo "script name : $0 " # script name means file name display means you want to give 0 
+
+echo "current working directory : $PWD"
+
+echo "current directory of the home user: $HOME"
+
+echo "which user is running this script : $USER"
+
+echo "host name : $HOSTNAME"
+
+echo "process id of the current shell-script : $$"
+
+echo ""
