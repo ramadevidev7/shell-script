1627b23a7d3069938b79a6b412389253e33c747c
ramadevidev7 <ramadevi.dev7@gmail.com>
2024-07-09 00:21:04 +0530

    new-files

--- 10-installation.sh
+++ 10-installation.sh
@@ -7,11 +7,18 @@ USERID=$(id -u )
 if [$USERID -ne 0]
 then 
 echo "please run this script with root access "
-
+exit 1 # manually exit if error comes
 else
 echo "you are the super user "
 fi
 
 dnf install mysql -y
 
+if [ $? -ne 0 ]
 
+then 
+echo "installation of mysql is failure"
+
+exit 1
+fi
+echo "is script proceeding"
