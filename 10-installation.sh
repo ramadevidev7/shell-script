bc9dedde8990bb24d0e88c31170ba70ba6e2775d
ramadevidev7 <ramadevi.dev7@gmail.com>
2024-07-09 00:23:29 +0530

    new-files

--- 10-installation.sh
+++ 10-installation.sh
@@ -4,7 +4,7 @@ USERID=$(id -u )
 
 # sudo userid is always 0 thats y we compare with 0 
 
-if [$USERID -ne 0]
+if [ $USERID -ne 0 ]
 then 
 echo "please run this script with root access "
 exit 1 # manually exit if error comes
@@ -14,11 +14,4 @@ fi
 
 dnf install mysql -y
 
-if [ $? -ne 0 ]
-
-then 
-echo "installation of mysql is failure"
-
-exit 1
-fi
 echo "is script proceeding"
