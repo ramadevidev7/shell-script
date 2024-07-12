cb351a43178de812cdf644bde06ecbde0086f18b
ramadevidev7 <ramadevi.dev7@gmail.com>
2024-07-12 23:29:42 +0530

    FUNCTIONS

--- 12-functions.sh
+++ 12-functions.sh
@@ -7,7 +7,7 @@ LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
 
 VALIDATE(){
-    if [ $1 -ne 0]
+    if [ $1 -ne 0 ]
     then 
     echo "$2... FAILURE"
     exit 1
@@ -17,7 +17,7 @@ VALIDATE(){
 
 }
 
-if [$USERID -ne 0 ]
+if [ $USERID -ne 0 ]
 then 
 echo "please run this script with root access."
 exit 1 # manually exit if error comess.
